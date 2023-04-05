7a95461a9be764b414a4a6f44f519a00e611de85
Hokkaidosm <64832302+Hokkaidosm@users.noreply.github.com>
2023-04-05 21:19:24 +0900

    GPL Header

--- twitter-archive-importer.php
+++ twitter-archive-importer.php
@@ -6,10 +6,27 @@ Description: This plugin provides a function for import Twitter archive file.
 Version: 0.1
 Author: Hokkaidosm
 Author URI: https://hokkaidosm.net
-Licence: MIT License
-Licence URI: https://spdx.org/licenses/MIT.html
+Licence: GPL v3 or later
+Licence URI: https://spdx.org/licenses/GPL-3.0-or-later.html
 Text Domain: twitter-archive-importer
 Domain Path: /languages
+ */
+
+/**
+ *     Copyright (C) 2023 Hokkaidosm
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 require_once(plugin_dir_path(__FILE__) . "constants.php");
